86112747232bd8bad0d1f52bde97ccdd68cd3a87
Luis Padron <luispadron25@gmail.com>
2024-02-18 21:51:38 -0500

    Get all shortened urls page

--- client/src/pages/shorten/Shorten.jsx
+++ client/src/pages/shorten/Shorten.jsx
@@ -1,9 +1,62 @@
-function Shorten({ props }) {
-  if (!props) {
-    return <h1>There is no data</h1>;
-  } else {
-    return <h1>{props}</h1>;
-  }
+import { useState, useEffect, useRef } from "react";
+import ThemeToggler from "../../components/ThemeToggler";
+import axios from "axios";
+import {
+  Heading,
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Tbody,
+} from "@chakra-ui/react";
+
+function Shorten() {
+  const [fullUrl, setFullUrl] = useState([]);
+  const dataFetchedRef = useRef(false);
+
+  const fetchAllUrl = async () => {
+    try {
+      const res = await axios.get("/url");
+      setFullUrl(res.data.data);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    fetchAllUrl();
+  }, []);
+
+  return (
+    <>
+      <ThemeToggler />
+      <Heading textAlign={"center"}>All URLs</Heading>
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>FullUrl</Th>
+              <Th>Short</Th>
+              <Th isNumeric>Clicks</Th>
+            </Tr>
+          </Thead>
+          {fullUrl.map((data) => (
+            <Tbody key={data._id}>
+              <Tr>
+                <Td>{data.fullUrl}</Td>
+                <Td>{data.shortUrl}</Td>
+                <Td isNumeric>{data.clicks}</Td>
+              </Tr>
+            </Tbody>
+          ))}
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
 
 export default Shorten;
